ce31398ecdeb49d629583a97c2354f4b003b3f22
RahulB <rahulbisht1012@gmail.com>
2023-04-05 12:02:23 +0530

    Home page Completed

---
+++ frontend/src/Components/shared/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './Button.module.css'
+
+const Button = ({ text }) => {
+    return (
+        <button className={styles.button}>
+            <span>{text}</span>
+            <img className={styles.arrow} src="/images/arrow-forward.png" alt="" />
+        </button>
+    )
+}
+
+export default Button
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+import Card from '../../Components/shared/Card/Card'
+import Button from '../../Components/shared/Button/Button'
+
+
+import styles from './Home.module.css'
+
 
 const Home = () => {
+
+    const signinLinkStyle = {
+        color: '#0077ff',
+        textDecoration: 'none',
+        fontWeight: '600',
+        marginLeft: '10px'
+    }
+
+
+
     return (
-        <div>Home</div>
+
+        <div className={`${styles.cardWrapper}`}>
+            <Card
+                title="Welcome to ClubHouse !"
+                icon="logo"
+            >
+                <p className={`${styles.text}`}>
+                    We’re working hard to get Club House ready for
+                    everyone! While we wrap up the finishing youches,
+                    we’re adding people gradually to make sure nothing breaks
+                </p >
+
+                <div>
+                    <Button text="Get Your UserName" />
+                </div>
+
+                <div className={styles.signinWrapper}>
+                    <span className={styles.hasInvite}>Have an invite text ?</span>
+                    <Link
+                        to={'/Login'}
+                        style={signinLinkStyle}
+                    >
+                        Sign In
+                    </Link>
+                </div>
+            </Card>
+        </div>
     )
 }
 
 export default Home
+
+
